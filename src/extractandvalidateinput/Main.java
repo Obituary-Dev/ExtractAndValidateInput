594fdf99cd359e055ea1499db50cd5b2a746e8db
Obituary-Dev <russianfreestyle@protonmail.com>
2021-02-24 10:50:28 +0100

    Update Main.java

--- src/extractandvalidateinput/Main.java
+++ src/extractandvalidateinput/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package extract.and.validate.input;
+package extractandvalidateinput;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
